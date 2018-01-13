90d6c05aaed008ac81cda3bb95985ad54202885c
Kit Masaracchia <kit.masaracchia@gmail.com>
2018-01-13 16:16:49 -0500

    add responsive navbar styling

--- client/src/main/Navbar.js
+++ client/src/main/Navbar.js
@@ -4,7 +4,8 @@ import glamorous from "glamorous";
 export default function Navbar() {
     ///////// STYLES \\\\\\\\\\
     const mediaQueries = {
-        medium: "@media screen and (max-width: 785px)",
+        small: "@media screen and (max-width: 414px)",
+        medium: "@media screen and (max-width: 768px)",
     }
     const Container = glamorous.div({
         display: "flex",
@@ -14,28 +15,41 @@ export default function Navbar() {
         height: "75px",
         backgroundColor: "#ECFF6A",
         width: "100%",
-        marginBottom: "100px",
+        margin: "0 auto 100px auto",
+        transition: "all 0.2s ease-in-out",
+        [mediaQueries.medium]: {
+            flexDirection: "column-reverse",
+            height: "190px",
+            padding: "25px",
+            margin: "0 auto 50px auto",
+        }
     });
 
     const Logo = glamorous.img({
-        height: "88.45px",
-        width: "444.22px",
+        height: "auto",
+        width: "444px",
         transform: "translateY(40%)",
+        transition: "all 0.2s ease-in-out",
+        [mediaQueries.medium]: {
+            height: "auto",
+            width: "300px",
+            transform: "translateY(0)",
+            marginTop: "25px",
+        }
     });
 
     const LinkContainer = glamorous.div({
         display: "flex",
         justifyContent: "space-around",
         width: "25%",
-        [mediaQueries.medium]: {
-            width: "100%",
-            justifyContent: "center",
-        }
     });
 
     const NavLink = glamorous.a({
         color: "#24251D",
         margin: "auto 10px",
+        [mediaQueries.medium]: {
+            margin: "auto 50px",
+        }
     });
     ////////////////////////////////////
 
